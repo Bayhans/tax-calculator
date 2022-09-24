b0fe1b1069e5f60050c807cc6c55e8fa81fdc1e7
Bayhans <88708850+Bayhans@users.noreply.github.com>
2022-09-24 12:42:29 +0200

    Add project files.

---
+++ TheTaxCalculator/App.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace TheTaxCalculator
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+          
+        }
+    }
+
+
+
+
+
+
+
+
+    //public string Convert(double amount, GoodsValue goodsValues)
+    //{
+    //    if (goodsValues == GoodsValue.NormalRate)
+    //        return (amount * 25 * 100).ToString("0.##");
+
+    //    if (goodsValues == GoodsValue.Foodstuffs)
+    //        return (amount * 15 * 100).ToString("0.##");
+
+    //    if (goodsValues == GoodsValue.OtherTax)
+    //        return (amount * 12 * 100).ToString("0.##");
+
+    //    throw new ArgumentOutOfRangeException("goodsValues");
+    //}
+
+    //public enum GoodsValue /*Or DistanceType*/
+    //{
+    //    NormalRate,
+    //    Foodstuffs,
+    //    OtherTax,
+    //}
+
+}
---
+++ TheTaxCalculator/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TheTaxCalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void CalculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            decimal total = 0;
+
+            if (ValueInput != null)
+            {
+                try
+                {
+                    decimal itemValueInput = Decimal.Parse(ValueInput.Text);
+                    if (NormalRate.IsSelected)
+                    {
+                        total += (itemValueInput * 25 * 100).ToString("0.##");
+                        TotalValue.Text = String.Format("{0:C}", total);
+                        ValueAddedTax.Text = String.Format("{0:C}", total);
+                    }
+                    if (FoodStuffRate.IsSelected)
+                    {
+                        (itemValue * 15 * 100).ToString("0.##");
+                        TotalValue.Text = String.Format("{0:C}", total);
+
+                    }
+                    if (OtherRate.IsSelected)
+                    {
+                        (itemValue * 12 * 100).ToString("0.##");
+                        TotalValue.Text = String.Format("{0:C}", total);
+
+                    }
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Please enter a valid amount in item price and tax rate");
+                }
+            }
+        }
+    }
+}
